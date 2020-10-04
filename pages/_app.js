029835056538ef31ac7a6206b79f1568cb671ce0
Aurelio Pita <jaapaurelio@gmail.com>
2020-10-04 11:43:08 +0100

    Scroll top on route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 import "../styles/globals.css";
 import PageHeader from "../components/PageHeader";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on("routeChangeComplete", () => {
+      window.scrollTo(0, 0);
+    });
+  });
+
   return (
-    <div style={{maxWidth: "900px", margin: '0 auto'}}>
+    <div style={{ maxWidth: "900px", margin: "0 auto" }}>
       <PageHeader></PageHeader>
       <Component {...pageProps} />
     </div>
