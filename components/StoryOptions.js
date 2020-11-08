f52c3ccfa8c13fc88c8eaf8ffa3d652569aeb447
Tiago Alves <letiagoalves@gmail.com>
2020-11-08 13:38:39 +0000

    reset scroll

--- components/StoryOptions.js
+++ components/StoryOptions.js
@@ -1,25 +1,34 @@
 import styles from './StoryOptions.module.css';
 import StoryOption from './StoryOption';
 import { CHARACTERS_COLORS, CHARACTERS_DARK_WHITELIST } from './../constants/story_constants';
+import { useEffect, useRef } from 'react';
 
 export default function StoryOptions({ currenOption, onOptionClick, showQuestion = true }) {
+    const carousselRef = useRef();
+    const currentCarouselLeftScroll = carousselRef.current ? carousselRef.current.scrollLeft : 0;
+
+    useEffect(() => {
+        if (currentCarouselLeftScroll > 0 && carousselRef.current) {
+            carousselRef.current.scrollLeft = 0;
+        }
+    }, [currentCarouselLeftScroll]);
+
     return (
         <div>
             <div className="pageWidthAlign">{showQuestion && <h2>{currenOption.question}</h2>}</div>
-            <div className={styles.caroussel}>
+            <div className={styles.caroussel} ref={carousselRef}>
                 {currenOption.options.map((option, i) => {
                     const textColor = CHARACTERS_DARK_WHITELIST.includes(option.character)
                         ? 'white'
                         : 'black';
                     return (
-                        <div key={i}>
-                            <StoryOption
-                                onClick={() => onOptionClick(option, i)}
-                                image={`/images/characters/${option.character}.svg`}
-                                backgroundColor={CHARACTERS_COLORS[option.character]}
-                                textColor={textColor}
-                                text={option.text}></StoryOption>
-                        </div>
+                        <StoryOption
+                            key={i}
+                            onClick={() => onOptionClick(option, i)}
+                            image={`/images/characters/${option.character}.svg`}
+                            backgroundColor={CHARACTERS_COLORS[option.character]}
+                            textColor={textColor}
+                            text={option.text}></StoryOption>
                     );
                 })}
             </div>
--- components/StoryResume.js
+++ components/StoryResume.js
@@ -2,6 +2,7 @@ import styles from './StoryResume.module.css';
 import StoryOption from '../components/StoryOption';
 import { useCallback } from 'react';
 import { useRouter } from 'next/router';
+import { CHARACTERS_COLORS, CHARACTERS_DARK_WHITELIST } from './../constants/story_constants';
 
 export default function StoryResume({ selectedStoryPlot, plots }) {
     const router = useRouter();
@@ -13,7 +14,6 @@ export default function StoryResume({ selectedStoryPlot, plots }) {
             const characterPos = selectedStoryPlot.indexOf(character);
             if (characterPos >= 0 && characterPos < selectedStoryPlot.length) {
                 const nStory = selectedStoryPlot.slice(0, characterPos);
-                console.log('nStory', nStory);
                 router.push({
                     pathname: '/aminhahistoria',
                     query: {
@@ -27,6 +27,7 @@ export default function StoryResume({ selectedStoryPlot, plots }) {
 
     const story = a.reduce((text, selectedOption, i) => {
         const option = currentOption.options.find((option) => option.character === selectedOption);
+        const textColor = CHARACTERS_DARK_WHITELIST.includes(option.character) ? 'white' : 'black';
 
         text = [
             ...text,
@@ -34,6 +35,8 @@ export default function StoryResume({ selectedStoryPlot, plots }) {
                 <StoryOption
                     image={`/images/characters/${option.character}.svg`}
                     text={option.text}
+                    backgroundColor={CHARACTERS_COLORS[option.character]}
+                    textColor={textColor}
                     readOnly={true}
                     onClick={() => onChangeCharacter(option.character)}></StoryOption>
             </div>,
